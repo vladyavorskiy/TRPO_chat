13d02df9d98b1b04f42c0fea490c50ec34c5be8b
Vladislav Yavorskij <yavorskij_vi@edu.surgu.ru>
2024-05-27 17:40:48 +0500

    Reply

--- front_js/my-vue-app/src/App.vue
+++ front_js/my-vue-app/src/App.vue
@@ -2,6 +2,22 @@
 import axios from "axios";
 import { ref, watch, onMounted } from "vue";
 import { HubConnectionBuilder, HubConnection } from "@microsoft/signalr";
+import { nextTick } from 'vue';
+
+type TriState = true | false | null;
+
+interface Message {
+  id: number;
+  text: string;
+  sender: string;
+  replyTo: ReplyInfo | null;
+}
+
+interface ReplyInfo {
+  messageReplyId: number;
+  replyState: boolean | null;
+}
+
 
 const userNameInput = ref("");
 const userName = ref("");
@@ -9,7 +25,11 @@ const currentGroup = ref<string>("");
 const newMessage = ref("");
 const newGroupName = ref<string>("");
 const availableGroups = ref<string[]>([]);
-const groupMessages = ref<{ text: string; sender: string }[]>([]);
+// const groupMessages = ref<{ text: string; sender: string; id: number; replyTo: TriState }[]>(
+//   []
+// );
+const groupMessages = ref<Message[]>([]);
+
 
 const connection = ref<HubConnection | null>(null);
 
@@ -29,32 +49,56 @@ const setUserName = async () => {
 const initializeSignalRConnection = async () => {
   connection.value = new HubConnectionBuilder()
     .withUrl("/hub", {
-      accessTokenFactory: () => encodeURIComponent(userName.value), // Encode the username
+      accessTokenFactory: () => encodeURIComponent(userName.value),
     })
     .build();
 
+  // connection.value.on(
+  //   "onMessage",
+  //   (message: Message) => {
+  //     if (currentGroup.value) {
+  //       groupMessages.value.push({
+  //       text: message.text,
+  //       sender: message.sender,
+  //       id: message.id,
+  //       replyTo: message.replyTo});
+  //     }
+  //     scrollToEnd();
+  //   }
+  // );
+
   connection.value.on(
     "onMessage",
-    (message: string, sender: string, id: int) => {
+    (message: Message) => {
       if (currentGroup.value) {
-        groupMessages.value.push({ text: message, sender, id });
+        groupMessages.value.push(message);
       }
       scrollToEnd();
     }
   );
 
-  connection.value.on("onUserJoin", (userName: string) => {
+  connection.value.on("onUserJoin", (userName: string, id: number) => {
     if (currentGroup.value) {
       const message = `${userName} has joined the group.`;
-      groupMessages.value.push({ text: message, sender: "System" });
+      groupMessages.value.push({
+        text: message,
+        sender: "System",
+        id: id,
+        replyTo: null,
+      });
       scrollToEnd();
     }
   });
 
-  connection.value.on("onUserRemoved", (userName: string) => {
+  connection.value.on("onUserRemoved", (userName: string, id: number) => {
     if (currentGroup.value) {
       const message = `${userName} has left the group.`;
-      groupMessages.value.push({ text: message, sender: "System" });
+      groupMessages.value.push({
+        text: message,
+        sender: "System",
+        id: id,
+        replyTo: null,
+      });
       scrollToEnd();
     }
   });
@@ -62,6 +106,16 @@ const initializeSignalRConnection = async () => {
   connection.value.on("onMessageRemoved", (messageId) => {
     groupMessages.value = groupMessages.value.filter(
       (message) => message.id !== messageId
+    );
+  });
+
+  connection.value.on("onGroupDeleted", (groupName: string) => {
+    if (currentGroup.value === groupName) {
+      currentGroup.value = "";
+      groupMessages.value = [];
+    }
+    availableGroups.value = availableGroups.value.filter(
+      (group) => group !== groupName
     );
   });
 
@@ -76,7 +130,7 @@ const initializeSignalRConnection = async () => {
   );
 
   connection.value.onclose(async () => {
-    await initializeSignalRConnection(); // Reinitialize connection on disconnect
+    await initializeSignalRConnection();
   });
 
   try {
@@ -86,7 +140,6 @@ const initializeSignalRConnection = async () => {
     console.error("Error starting SignalR connection:", err);
   }
 
-  // Update connection ID if user already exists
   if (userName.value) {
     await setUserConnection();
   }
@@ -108,12 +161,14 @@ const setUserConnection = async () => {
           connectionId: connection.value.connectionId,
         },
       });
-      await loadAvailableGroups(); // Load groups after setting the user connection
+      await loadAvailableGroups();
     } catch (error) {
       console.error("Error setting user connection:", error);
     }
   }
 };
+
+const isGroupCreator = ref(false);
 
 const joinGroup = async (group: string) => {
   if (userName.value) {
@@ -123,23 +178,57 @@ const joinGroup = async (group: string) => {
       });
       currentGroup.value = group;
       await loadGroupMessages(group);
+
+      const response = await axios.get("/api/Chat/GetGroupCreator", {
+        params: { groupName: group },
+      });
+      isGroupCreator.value = response.data === userName.value;
     } catch (error) {
       console.error("Error joining group:", error);
     }
   }
 };
 
+const deleteGroup = async () => {
+  if (currentGroup.value && userName.value) {
+    try {
+      const response = await axios.post("/api/Chat/DeleteGroup", null, {
+        params: { groupName: currentGroup.value, userName: userName.value },
+      });
+      console.log("Response from server:", response.data);
+      currentGroup.value = "";
+      groupMessages.value = [];
+      await loadAvailableGroups();
+    } catch (error) {
+      console.error("Error deleting group:", error);
+    }
+  }
+};
+
+let repliedMessageId = ref(null); 
+
+const replyMessage = async (message) => {
+  if (repliedMessageId.value === message.id) {
+    repliedMessageId.value = null;
+  } else {
+    repliedMessageId.value = message.id;
+  }
+};
+
 const sendMessage = async () => {
   if (currentGroup.value && userName.value) {
     try {
-      await axios.post("/api/Chat/SendMessage", null, {
-        params: {
-          groupName: currentGroup.value,
-          message: newMessage.value,
-          userName: userName.value,
-        },
-      });
+      const params = {
+        groupName: currentGroup.value,
+        message: newMessage.value,
+        userName: userName.value,
+      };
+      if (repliedMessageId.value) {
+        params["messageId"] = repliedMessageId.value;
+      }
+      await axios.post("/api/Chat/SendMessage", null, { params });
       newMessage.value = "";
+      repliedMessageId.value = null;
       scrollToEnd();
     } catch (error) {
       console.error("Error sending message:", error);
@@ -168,7 +257,7 @@ const leaveGroup = async () => {
       console.log("Response from server:", response.data);
       currentGroup.value = "";
       groupMessages.value = [];
-      await loadAvailableGroups(); // Refresh available groups after leaving
+      await loadAvailableGroups(); 
     } catch (error) {
       console.error(
         "Error leaving group:",
@@ -182,9 +271,8 @@ const createGroup = async () => {
   if (newGroupName.value.trim() !== "") {
     try {
       await axios.post("/api/Chat/CreateGroup", null, {
-        params: { groupName: newGroupName.value },
+        params: { groupName: newGroupName.value, creator: userName.value },
       });
-      // After creating a new group, add it to availableGroups
       availableGroups.value.push(newGroupName.value);
       await joinGroup(newGroupName.value);
       newGroupName.value = "";
@@ -224,33 +312,48 @@ const removeMessageForAll = async (messageId) => {
   }
 };
 
-const editedMessage = ref(""); // Store the edited message
-const isEditing = ref(false); // Flag to track editing mode
-let editedMessageId = null; // S
-const showDropdownMenuForMessage = ref(null);
+const editedMessage = ref(""); 
+const isEditing = ref(false); 
+let editedMessageId = null; 
+
+// const showDropdownMenuForMessage = ref(null);
+
+// const showDropdownMenu = (message) => {
+//   // Показываем меню только для сообщений текущего пользователя
+//   if (message.sender === userName.value) {
+//     if (showDropdownMenuForMessage.value === message.id) {
+//       // Если меню уже показано для этого сообщения, скрываем его
+//       showDropdownMenuForMessage.value = null;
+//     } else {
+//       // Показываем меню для выбранного сообщения
+//       showDropdownMenuForMessage.value = message.id;
+//     }
+//   }
+// };
+
+const showDropdownMenuForMessage = ref<number | null>(null);
 
 const showDropdownMenu = (message) => {
-  // Показываем меню только для сообщений текущего пользователя
-  if (message.sender === userName.value) {
-    if (showDropdownMenuForMessage.value === message.id) {
-      // Если меню уже показано для этого сообщения, скрываем его
-      showDropdownMenuForMessage.value = null;
-    } else {
-      // Показываем меню для выбранного сообщения
-      showDropdownMenuForMessage.value = message.id;
-    }
+  if(message.sender == 'System'){
+    showDropdownMenuForMessage.value = null;
+  }
+  else if (showDropdownMenuForMessage.value === message.id) {
+    showDropdownMenuForMessage.value = null;
+  } else {
+    showDropdownMenuForMessage.value = message.id;
   }
 };
 
 const editMessage = async (message) => {
-  isEditing.value = true; // Enter editing mode
-  editedMessage.value = message.text; // Populate the input with the message text
-  editedMessageId = message.id; // Store the ID of the message being edited
+  isEditing.value = true; 
+  editedMessage.value = message.text;
+  editedMessageId = message.id; 
 };
 
 const cancelEdit = () => {
-  editedMessage.value = ""; // Clear the edited message
-  isEditing.value = false; // Exit editing mode
+  editedMessage.value = "";
+  isEditing.value = false; 
+  showDropdownMenuForMessage.value = null;
 };
 
 const saveEdit = async () => {
@@ -271,6 +374,43 @@ const saveEdit = async () => {
     }
   }
 };
+
+const scrollToMessage = (messageId) => {
+  nextTick(() => {
+    const messageElement = document.getElementById(`message-${messageId}`);
+    if (messageElement) {
+      messageElement.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  });
+};
+
+const scrollToOriginalMessage = (message) => {
+  console.log("scrollToOriginalMessage:");
+  if (message.replyTo !== null) {
+    console.log("Original message ID:", `message-${message.replyTo.messageReplyId}`);
+    const originalMessage = groupMessages.value.find(
+      (msg) => msg.id === message.replyTo.messageReplyId
+    );
+    console.log("Original message:", originalMessage);
+    if (originalMessage) {
+      nextTick(() => {
+        const originalMessageElement = document.querySelector(`[data-id='message-${originalMessage.id}']`);
+        console.log("Original message element:", originalMessageElement);
+        if (originalMessageElement) {
+            originalMessageElement.scrollIntoView({ behavior: "smooth", block: "center" });
+            originalMessageElement.classList.add('highlight'); 
+            setTimeout(() => {
+              originalMessageElement.classList.remove('highlight'); 
+            }, 1000);
+          }
+      });
+    }
+  }
+};
+
+
+
+
 
 onMounted(async () => {
   if (userName.value) {
@@ -322,42 +462,46 @@ onMounted(async () => {
             <div
               v-for="message in groupMessages"
               :key="message.id"
-              class="message"
-              :class="{
-                'user-message': message.sender === userName,
-                'other-message': message.sender !== userName,
-                'system-message': message.sender === 'System' 
-              }"
+              :data-id="'message-' + message.id"
+              :class="[
+                'message',
+                {
+                  'user-message': message.sender === userName,
+                  'other-message': message.sender !== userName,
+                  'system-message': message.sender === 'System',
+                  'choice-reply-message': repliedMessageId == message.id && repliedMessageId != null,
+                  'other-reply-to-message': (message.replyTo && message.replyTo.replyState === false && message.sender == userName) || (message.replyTo && message.replyTo.replyState === true && message.sender != userName),
+                  'user-reply-to-message': (message.replyTo && message.replyTo.replyState === false && message.sender != userName) || (message.replyTo && message.replyTo.replyState === true && message.sender == userName),
+                },
+              ]"
+
               @contextmenu.prevent.right="showDropdownMenu(message)"
+              @dblclick="scrollToOriginalMessage(message)"
             >
               <span v-if="message.sender !== userName" class="sender-name"
                 >{{ message.sender }}:</span
               >
+
               {{ message.text }}
 
-              
               <div
-                v-if="showDropdownMenuForMessage === message.id"
+                v-if="showDropdownMenuForMessage === message.id &&  message.replyTo == null"
                 class="dropdown"
               >
-                <button @click="removeMessageForAll(message.id)">
+                <button
+                  v-if="message.sender === userName"
+                  @click="removeMessageForAll(message.id)"
+                >
                   Удалить
                 </button>
-                <button @click="editMessage(message)">Редактировать</button>
+                <button
+                  v-if="message.sender === userName"
+                  @click="editMessage(message)"
+                >
+                  Редактировать
+                </button>
+                <button @click="replyMessage(message)">Ответить</button>
               </div>
-
-              <!-- <button
-                v-if="message.sender === userName" 
-                @click="removeMessageForAll(message.id)"
-              >
-                Remove for All
-              </button>
-              <button
-                v-if="message.sender === userName"
-                @click="editMessage(message)"
-              >
-                Edit
-              </button> -->
             </div>
           </div>
           <div class="input-container" v-if="!isEditing">
@@ -370,12 +514,14 @@ onMounted(async () => {
             <button type="submit" @click="sendMessage">Send</button>
           </div>
           <div class="buttons-container">
+            <button v-if="isGroupCreator" @click="deleteGroup">
+              Delete Group
+            </button>
             <button @click="loadAvailableGroups">Back to Menu</button>
             <button @click="leaveGroup">Leave Group</button>
           </div>
         </div>
         <div class="input-container" v-if="isEditing">
-          <!-- Editing mode -->
           <input
             v-focus
             v-model="editedMessage"
@@ -450,10 +596,25 @@ onMounted(async () => {
   text-align: left;
 }
 
-.system-message{
-  text-align: center;
-  color: #ab2424;
+.user-reply-to-message{
+  text-align: right;
+  background-color: #869898cc;
+  margin-right: 20px; 
+}
 
+.other-reply-to-message{
+  text-align: left;
+  background-color: #869898cc;
+  margin-left: 20px; 
+}
+
+.system-message {
+  text-align: center;
+  color: #24ab65;
+}
+
+.choice-reply-message{
+  color: rgb(255, 0, 0);
 }
 
 .input-container {
@@ -470,4 +631,10 @@ onMounted(async () => {
   top: 120px;
   right: 10px;
 }
+
+.highlight {
+  background-color: #ffc107; 
+}
 </style>
+
+
--- front_js/my-vue-app/vite.config.js
+++ front_js/my-vue-app/vite.config.js
@@ -3,20 +3,26 @@ import { defineConfig } from 'vite'
 
 const backend = "https://localhost:7008";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "^/api": {
-        target: backend,
-        ws: false,
-        secure: false,
-      },
-      "^/hub": {
-        target: backend,
-        ws: true,
-        secure: false,
-      },
-    },
-  },
+const new_backend = "http://backend";
+// const backend = "https://localhost:2080";
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) =>{
+  return{
+ plugins: [vue()],
+ server: {
+   host: "0.0.0.0",
+   proxy: {
+     "^/api": {
+       target: mode == "development" ? backend : new_backend,
+       ws: false,
+       secure: false,
+     },
+     "^/hub": {
+       target: mode == "development" ? backend : new_backend,
+       ws: true,
+       secure: false,
+     },
+   },
+ },
+}
 });
